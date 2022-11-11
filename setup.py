85c4f7431f04ae1c279b4fb35e6be433e0113dd2
priv-kweihmann <priv-kweihmann@localhost>
2022-11-11 14:27:01 +0000

    Release 1.8.1

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open('requirements-dev.txt') as f:
 
 setuptools.setup(
     name='shellexeclist',
-    version='1.8.0',
+    version='1.8.1',
     author='Konrad Weihmann',
     author_email='kweihmann@outlook.com',
     description='list all executables called by a shell script',
